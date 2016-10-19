347b2ecfeda9808ed2a06da7a1381a391be73d4f
ndvssankar <sivashankar@msitprogram.net>
2016-10-19 19:20:00 +0530

    Deleted the unwanted files

--- processor/src/main/java/com/log/processor/db/Sample.java
+++
@@ -1,9 +0,0 @@
-public class Sample {
-	public static void main(String[] args) {
-		String key = "Hello";
-		String newKey = key;
-		newKey = newKey + " Good ";
-		System.out.println(key);
-		System.out.println(newKey);
-	}
-}
